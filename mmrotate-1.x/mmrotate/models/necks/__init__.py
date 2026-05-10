41f5a6940b21bc4fafb9ab0b16f9dc2c3b8522cb
ssdle <162157220+ssdle@users.noreply.github.com>
2026-05-10 21:55:34 +0800

    Update __init__.py

---
+++ mmrotate-1.x/mmrotate/models/necks/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .re_fpn import ReFPN
+from .fpnformer_retinanet import FPNdecoderformer_swin_double
+from .fpnformer_retinanet_ssdd import FPNformer_ssdd
+from .
+
+__all__ = ['ReFPN', 'FPNdecoderformer_swin_double', 'FPNformer_ssdd']
